301bf84983991cdd74e6d12dd4a1de1fc8fab969
José Refugio Melecio Soto <jose.melecio@digitalife.com.mx>
2023-03-08 16:43:56 -0600

    Conection to postgresql

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,5 +1,33 @@
 import { Global, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService, ConfigType } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import config from 'src/config/config';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
 @Global()
-@Module({})
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      load: [config],
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService<ConfigType<typeof config>>) => {
+        const dbConfig = configService.get('database');
+        const options: PostgresConnectionOptions = {
+          type: 'postgres',
+          host: '192.168.200.206', //dbConfig.host,
+          port: 5432, //dbConfig.port,
+          username: 'root', //dbConfig.user,
+          password: 'secret1234', //dbConfig.password,
+          database: 'posware', //dbConfig.name,
+          entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+          synchronize: false,
+        };
+        return options;
+      },
+      inject: [ConfigService],
+    }),
+  ],
+  exports: [TypeOrmModule],
+})
 export class DatabaseModule {}
