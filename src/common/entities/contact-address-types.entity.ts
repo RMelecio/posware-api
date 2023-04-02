e814c417d3dce8f48346c43396eeff0beec3d4b3
Jose Refugio Melecio Soto <jvl.melecio@gmail.com>
2023-04-02 00:01:34 -0600

    Status Table

--- src/common/entities/contact-address-types.entity.ts
+++ src/common/entities/contact-address-types.entity.ts
@@ -3,7 +3,7 @@ import { Column, Entity } from 'typeorm';
 import { BaseEntity } from './base.entity';
 
 @Entity({ name: 'contact_address_types' })
-export class ContactAddressType extends BaseEntity{
+export class ContactAddressType extends BaseEntity {
   @Column()
   name: string;
 }
